aed9517a9b5dd4c1a9fcdfcd89208d9e9433bf07
Alexi Gonzalez C <Alexi Gonzalez C@Alexi>
2016-10-23 00:46:28 -0300

    hola ya esta

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+﻿function Ajax(url, divRes, param) {
+    //alert(param);
+    $.ajax({
+        type: "POST",
+        cache: 'false',
+        url: url,
+        data: {'param': param},
+        success: function(data) {
+            //alert(data);
+            $(divRes).html(data);
+        }
+    });
+}
