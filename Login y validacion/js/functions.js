88bf514ebbbeef0ea6c338be4c8b8176685f5e3c
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2016-10-25 13:51:42 -0300

    Todos los archivos estan en carpeta "Archivos"

---
+++ Login y validacion/js/functions.js
@@ -0,0 +1,27 @@
+﻿function ValidarLogin(form){
+	if(form.user.value == ""){
+		alert("Debe ingresar un usuario.");
+		form.user.focus();
+		return false;
+	}
+	if(form.password.value == ""){
+		alert("Ingrese su contraseña");
+		form.password.focus();
+		return false;
+	}
+	return true;
+}
+
+function Ajax(url, divRes, param) {
+    //alert(param);
+    $.ajax({
+        type: "POST",
+        cache: 'false',
+        url: url,
+        data: {'param': param},
+        success: function(data) {
+            //alert(data);
+            $(divRes).html(data);
+        }
+    });
+}
