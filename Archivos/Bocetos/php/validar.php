29e048396932c58852544d94ac6417a49b23d47d
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2016-10-25 14:37:09 -0300

    Todos los archivos estan en carpeta "Archivos"

---
+++ Archivos/Bocetos/php/validar.php
@@ -0,0 +1,65 @@
+﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<?php
+#este script hara la conexion y redigira hacia la ventana indicada dependiendo
+#de los datos del usuario
+session_start();
+include("conex.inc");
+
+$usuario = $_POST["user"];
+$password = md5($_POST["password"]);
+$query = "SELECT id_cargo FROM usuarios WHERE rut = '$usuario' AND password = '$password'";
+$result = mysql_query($query, $db);
+$count = mysql_numrows($result);
+
+if ($count == 1) {
+    echo $row = mysql_fetch_row($result);
+	
+    if ($row[0] == "1") {
+
+        $query1= "SELECT nombre, apellido, rut FROM personas WHERE rut = '$usuario'";
+        $result1 = mysql_query($query1, $db);
+        $row1 = mysql_fetch_row($result1);
+        $_SESSION['estado'] = "logueadocaja";
+        $_SESSION['rut'] = "$row1[2]";
+        $_SESSION['nombrecompleto'] = "$row1[0] $row1[1]";
+        header("Location: ../php/Caja.php");				#es caja
+	}
+    if ($row[0] == "2") {
+
+        $query1= "SELECT nombre, apellido, rut FROM personas WHERE rut = '$usuario'";
+        $result1 = mysql_query($query1, $db);
+        $row1 = mysql_fetch_row($result1);
+        $_SESSION['estado'] = "logueadoinv";
+        $_SESSION['rut'] = "$row1[2]";
+        $_SESSION['nombrecompleto'] = "$row1[0] $row1[1]";
+        header("Location: ../php/Inventario.php");					#es inventario
+    }
+    if ($row[0] == "3") {
+
+        $query1= "SELECT nombre, apellido, rut FROM personas WHERE rut = '$usuario'";
+        $result1 = mysql_query($query1, $db);
+        $row1 = mysql_fetch_row($result1);
+        $_SESSION['estado'] = "logueadoprov";
+        $_SESSION['rut'] = "$row1[2]";
+        $_SESSION['nombrecompleto'] = "$row1[0] $row1[1]";
+        header("Location: ../php/Proveedor.php");					#es proveedores
+    }
+	
+	if ($row[0] == "4") {
+
+        $query1= "SELECT nombre, apellido, rut FROM personas WHERE rut = '$usuario'";
+        $result1 = mysql_query($query1, $db);
+        $row1 = mysql_fetch_row($result1);
+        $_SESSION['estado'] = "logueadoadm";
+        $_SESSION['rut'] = "$row1[2]";
+        $_SESSION['nombrecompleto'] = "$row1[0] $row1[1]";
+        header("Location: ../php/Administrador.php");					#es admin
+    }
+    
+    
+} else {
+    //Redireccionar a la pagina de inicio en caso de error
+    header("Location: ../index.php?msj=Usuario+o+contraseña+Inválida");
+}
+
+?>
---
+++ Archivos/Bocetos/php/view/boletas.php
@@ -0,0 +1,44 @@
+<input id="tab-1" type="radio" name="tab-group" checked="checked" />		<!--Pestaña 1 activa por defecto-->
+<label for="tab-1">Boletas</label>						
+<input id="tab-2" type="radio" name="tab-group" />							<!--Pestaña 2 inactiva por defecto-->
+<label for="tab-2">Ver detalles</label>						
+<input id="tab-3" type="radio" name="tab-group" />							<!--Pestaña 3 inactiva por defecto-->
+<label for="tab-3">Ajustes</label>					
+<div id="content">															<!--Contenido a mostrar/ocultar-->							
+	<div id="content-1">													<!--Contenido de la Pestaña 1-->
+		<table>
+			<tr>
+				<td><img class="img-title-tab" src="img/menu/boletas.png" width="40px" height="40px" /></td>
+				<td><p class="title-tab">Boletas</p></td>
+			</tr>
+		</table>
+		<hr class="separator-title">
+		<p class="content-info">
+			<?php include("../textodemo.php");?>
+		</p>
+	</div>
+	<div id="content-2">													<!--Contenido de la Pestaña 2-->
+		<table>
+			<tr>
+				<td><img class="img-title-tab" src="img/menu/boletas.png" width="40px" height="40px" /></td>
+				<td><p class="title-tab">Ver detalles</p></td>
+			</tr>
+		</table>
+		<hr class="separator-title">
+		<p class="content-info">
+			<?php include("../textodemo.php");?>
+		</p>
+	</div>
+	<div id="content-3">													<!--Contenido de la Pestaña 3-->
+		<table>
+			<tr>
+				<td><img class="img-title-tab" src="img/menu/boletas.png" width="40px" height="40px" /></td>
+				<td><p class="title-tab">Ajustes</p></td>
+			</tr>
+		</table>
+		<hr class="separator-title">
+		<p class="content-info">
+			<?php include("../textodemo.php");?>
+		</p>	
+	</div>
+</div>
