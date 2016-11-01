5d84d088d72c896524884c651f160dda25da3f50
Alexi Gonzalez C <Alexi Gonzalez C@172.16.124.200>
2016-11-01 20:01:55 -0300

    ya gonzalo

--- php/view/conStock.php
+++ php/view/conStock.php
@@ -1,5 +1,5 @@
 <input id="tab-1" type="radio" name="tab-group" checked="checked" />		<!--Pestaña 1 activa por defecto-->
-<label for="tab-1">Con Stock</label>						
+<!--<label for="tab-1">Con Stock</label>-->						
 <div id="content">															<!--Contenido a mostrar/ocultar-->							
 	<div id="content-1">													<!--Contenido de la Pestaña 1-->
 		<table>
