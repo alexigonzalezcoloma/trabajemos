3a4458205778a833fec8ef83cb849f7fd713ac47
Alexi Gonzalez C <Alexi Gonzalez C@172.16.124.200>
2016-11-08 11:07:19 -0300

    nuestro modulo

--- boceto1.php
+++ boceto1.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" type="text/css" href="css/tabla.css"/>
         <link rel="stylesheet" type="text/css" href="css/responsive.css"/>
 		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/functions-inventario.js"></script>
 		<script type="text/javascript" src="js/functions-js.js"></script>
 		<script type="text/javascript" src="js/functions-jquery.js"></script>
 		
@@ -69,12 +70,17 @@
 
 						<center><form action="boceto1.php">
 							INGRESAR 'ID' DEL PRODUCTO: 
-				            <input type="text" name="busca" va>
-				            <input type="submit" name="buscar" value="Buscar">
-				        </form></center>
-				        <br/>
-
-			        <?php
+				            <input type="text" id="busca" name="busca">
+				            <button id="buscar" name="buscar">Buscar</button>
+							<br/>
+							<div id="mi respuesta"></div>
+							INGRESAR 'NOMBRE' DEL PRODUCTO:
+							<input type="text" name="nombre">
+				            <input type="submit" name="nombres" value="Buscar">
+							<br/>	
+						</form></center>
+									
+					<?php
 						if(isset($_REQUEST["busca"])){
 							$conexion = mysql_connect("localhost","root","");
 							mysql_select_db("valeria");
@@ -110,6 +116,43 @@
 							}
 							echo "</table>";
 							mysql_close($conexion);
+						}
+						if(isset($_REQUEST["nombre"])){
+							$conexion = mysql_connect("localhost","root","");
+							mysql_select_db("valeria");
+							$buscar2 = $_GET["nombre"];
+							$sql = "SELECT * FROM productos WHERE nom_producto='$buscar2'"; 
+
+							$resultado2 = mysql_query($sql,$conexion);
+							echo "	<table id='table-div' cellspacing='0' cellpadding='0' border='0' width='325'>
+										<tr>
+											<th>ID Producto</th>
+											<th>Nombre Producto</th>
+											<th>Stock</th>
+											<th>Precio Neto</th>
+											<th>Precio</th>
+											<th>Lista Compra</th>
+											<th>Descripcion</th>
+										</tr>";
+							if(mysql_num_rows($resultado2) > 0){
+								while($fila = mysql_fetch_array($resultado2)){
+									echo"<tr>
+											<td>$fila[0]</td>				
+											<td>$fila[1]</td>
+											<td>$fila[2]</td>
+											<td>$fila[3]</td>					
+											<td>$fila[4]</td>					
+											<td>$fila[5]</td>
+											<td>$fila[6]</td>
+										</tr>";
+								}
+							}
+							else{
+								echo"<td colspan='8'>Sin resultados</td>";
+							}
+							echo "</table>";
+							mysql_close($conexion);
+							
 						}
 					?>
 					</p>
