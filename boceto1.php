22929fa660aa8fe360b42d3f04e7e71ceddbe15f
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2016-11-20 17:16:22 -0300

    Add files via upload
    Validado

--- boceto1.php
+++ boceto1.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="js/validaciones.js"></script>
 <?php
 	session_start();
 	if($_SESSION['estado'] == "logueadoinv"){
@@ -13,14 +14,14 @@
 					<script type='text/javascript' src='js/jquery-3.1.1.js'></script>
 					<script type='text/javascript' src='js/funcionesbuscar.js'></script>
 					<script type='text/javascript' src='js/funciones-inventario.js'></script>
-					
+					<script type='text/javascript' src='js/validaciones.js'></script>
 					<link rel='icon' type='image/png' href='img/icon.png' />
-					<meta name='viewport' content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no' />	
+					<meta name='viewport' content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no' />
 				</head>
 				<body>
-				<div id='webpage'>	
+				<div id='webpage'>
 					<div id='webpage-lateral'>
-						<div id='webpage-lateral-menu'>		
+						<div id='webpage-lateral-menu'>
 							<img src='img/logo.png' style='margin:0 auto;margin-top:5px;'/><br/><br/>
 							<label style='font-size:8pt;margin-left:5px;'>Conectado como: Inventario</label>
 							<br/><br/>
@@ -55,10 +56,10 @@
 							</div>
 						</div>
 					</div>
-					<div id='webpage-body'>			
+					<div id='webpage-body'>
 						<input id='tab-1' type='radio' name='tab-group' checked='checked' />		<!--Pestaña 1 activa por defecto-->
-						<!--<label id='text-tab-1' for='tab-1'>Inicio</label>-->								
-						<div id='content'>															<!--Contenido a mostrar/ocultar-->					
+						<!--<label id='text-tab-1' for='tab-1'>Inicio</label>-->
+						<div id='content'>															<!--Contenido a mostrar/ocultar-->
 							<div id='content-1'>													<!--Contenido de la Pestaña 1-->
 								<table>
 									<tr>
@@ -74,16 +75,16 @@
 												<th>Buscar Productos</th>
 											</tr>
 										</table><br/>
-										
-									
-										<center>
-											INGRESAR CÓDIGO DEL PRODUCTO: 
-											<input type='number' min='1' id='producto-codigo'>
-											<input type='button' id='busca' value='Buscar'>
-										</center>
+
+
+										<center><form name='formulario'>
+										INGRESAR CÓDIGO DEL PRODUCTO: 
+										<input type='text' id='producto-codigo' name='producto-codigo' placeholder='solo numeros' onkeypress='return solonumeros(event)'/>
+										<input type='button' id='busca' value='Buscar'/>
+										</form></center>
 										<br/>
 									</p>
-								</div>	
+								</div>
 							</div>
 						</div>
 					</div>
